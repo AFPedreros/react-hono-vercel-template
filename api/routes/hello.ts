124b1d3ec8aff02f2e81473b6c5cf6c48229be54
afpedreros <felipe.pedreros94@gmail.com>
2024-07-14 16:09:38 -0500

    refactor(api): update hello route to root path

--- api/routes/hello.ts
+++ api/routes/hello.ts
@@ -1,7 +1,15 @@
 import { Hono } from "hono";
 
-export const helloRoute = new Hono().get("/", (c) => {
-	return c.json({
-		message: "Hello from Hono!",
+import.meta.env = process.env;
+
+export const helloRoute = new Hono()
+	.get("/", (c) => {
+		return c.json({
+			message: "Hello from Hono!",
+		});
+	})
+	.get("/env", (c) => {
+		return c.json({
+			message: `${import.meta.env.VERCEL}`,
+		});
 	});
-});
