c9ca9c8d9975bdd86b547614737c56b1e1fdba37
afpedreros <felipe.pedreros94@gmail.com>
2024-07-14 17:02:35 -0500

    refactor(api): update server configuration to use lazy loading for routes

--- api/[[...route]].ts
+++ api/[[...route]].ts
@@ -2,7 +2,6 @@ import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { handle } from "hono/vercel";
 import { helloRoute } from "./routes/hello";
-import { serve } from "@hono/node-server";
 
 export const config = {
 	runtime: "edge",
