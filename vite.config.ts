679b6558150fca198189fdf2c2de26614416c593
afpedreros <felipe.pedreros94@gmail.com>
2024-07-15 07:07:02 -0500

    chore: Remove unused dependencies and plugins from package.json and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,11 @@
 import { defineConfig, loadEnv } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react-swc";
-import devServer from "@hono/vite-dev-server";
-import { visualizer } from "rollup-plugin-visualizer";
-import simpleHtmlPlugin from "vite-plugin-simple-html";
 
 process.env = { ...process.env, ...loadEnv("production", process.cwd(), "") };
 
 export default defineConfig({
-	plugins: [
-		react(),
-		simpleHtmlPlugin({
-			minify: true,
-		}),
-		!("VERCEL" in process.env) && visualizer(),
-	],
+	plugins: [react()],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
