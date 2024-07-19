b9ad4bcbb823dedd02b306a509c4e8665c0c9a1d
afpedreros <felipe.pedreros94@gmail.com>
2024-07-19 16:23:01 -0500

    chore: Update dev scripts and dependencies for API and client development

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,68 @@
 // eslint-disable-next-line import/order
 import path from "node:path";
+import devServer from "@hono/vite-dev-server";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig, loadEnv } from "vite";
 
 process.env = { ...process.env, ...loadEnv("production", process.cwd(), "") };
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@api": path.resolve(__dirname, "./api"),
-    },
+const resolve = {
+  alias: {
+    "@": path.resolve(__dirname, "./src"),
+    "@api": path.resolve(__dirname, "./api"),
   },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
+};
+
+export default defineConfig(async ({ mode }) => {
+  if (mode === "client") {
+    return {
+      plugins: [react()],
+      resolve,
+      server: {
+        proxy: {
+          "/api": {
+            target: "http://localhost:3000",
+            changeOrigin: true,
+            rewrite: (path: string) => path.replace(/^\/api/, "/api"),
+          },
+        },
       },
-    },
-  },
+    };
+  } else {
+    const isDev = process.env.NODE_ENV === "development";
+    let devServerPlugin;
+
+    if (isDev) {
+      // const { env, dispose } = await getPlatformProxy();
+      devServerPlugin = devServer({
+        entry: "./api/[[...route]].ts",
+      });
+    }
+
+    return {
+      plugins: [devServerPlugin],
+      resolve,
+      server: {
+        port: 3000,
+      },
+    };
+  }
 });
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//       "@api": path.resolve(__dirname, "./api"),
+//     },
+//   },
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: "http://localhost:3000",
+//         changeOrigin: true,
+//       },
+//     },
+//   },
+// });
