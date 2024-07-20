f8f7f9d93d4ac6608b66b13b284cabe359456176
afpedreros <felipe.pedreros94@gmail.com>
2024-07-19 20:30:14 -0500

    chore: Update dev scripts and dependencies for API and client development, and remove unused dependencies and plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,68 +1,22 @@
 // eslint-disable-next-line import/order
 import path from "node:path";
-import devServer from "@hono/vite-dev-server";
-import viteReact from "@vitejs/plugin-react";
 import react from "@vitejs/plugin-react-swc";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 
-process.env = { ...process.env, ...loadEnv("production", process.cwd(), "") };
-
-const resolve = {
-  alias: {
-    "@": path.resolve(__dirname, "./src"),
-    "@api": path.resolve(__dirname, "./api"),
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@api": path.resolve(__dirname, "./api"),
+    },
   },
-};
-
-export default defineConfig(async ({ mode }) => {
-  if (mode === "client") {
-    return {
-      plugins: [viteReact()],
-      resolve,
-      server: {
-        proxy: {
-          "/api": {
-            target: "http://localhost:3000",
-            changeOrigin: true,
-            rewrite: (path: string) => path.replace(/^\/api/, "/api"),
-          },
-        },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
       },
-    };
-  } else {
-    const isDev = process.env.NODE_ENV === "development";
-    let devServerPlugin;
-
-    if (isDev) {
-      devServerPlugin = devServer({
-        entry: "./api/[[...route]].ts",
-      });
-    }
-
-    return {
-      plugins: [devServerPlugin],
-      resolve,
-      server: {
-        port: 3000,
-      },
-    };
-  }
+    },
+  },
 });
-
-// export default defineConfig({
-//   plugins: [react()],
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//       "@api": path.resolve(__dirname, "./api"),
-//     },
-//   },
-//   server: {
-//     proxy: {
-//       "/api": {
-//         target: "http://localhost:3000",
-//         changeOrigin: true,
-//       },
-//     },
-//   },
-// });
